51e87c826607051a4f69e7bf0a9dd31198c58cc3
webcat12345 <webcat91@gmail.com>
2017-08-12 05:45:32 +0200

    sign up page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the LoginPage page.
+ * Generated class for the SignupPage page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
@@ -10,20 +10,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
 })
-export class LoginPage {
+export class SignupPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    console.log('ionViewDidLoad SignupPage');
   }
 
-  openSignupPage() {
-    this.navCtrl.push('SignupPage');
+  openSigninPage() {
+    this.navCtrl.push('LoginPage')
   }
 
 }
